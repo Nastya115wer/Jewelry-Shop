58fac0677c885bf9b0bd8eb6f6d4442207ad1b06
Nastya115wer <nastya.svertokina.03@bk.ru>
2026-09-21 20:55:37 +0300

    Create jewelry.js

---
+++ data/jewelry.js
@@ -0,0 +1,98 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Кольцо «Вечность»",
+    category: "rings",
+    material: "Золото 585",
+    gemstone: "Бриллиант 0.5ct",
+    price: 89900,
+    stock: 12,
+    image: "https://images.unsplash.com/photo-1605100804763-247f67b3557e?w=400",
+    description: "Классическое обручальное кольцо из золота с бриллиантом",
+    rating: 4.9
+  },
+  {
+    id: 2,
+    name: "Серьги «Капля»",
+    category: "earrings",
+    material: "Белое золото 585",
+    gemstone: "Сапфир 1.2ct",
+    price: 124500,
+    stock: 5,
+    image: "https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=400",
+    description: "Изысканные серьги-подвески с натуральными сапфирами",
+    rating: 4.8
+  },
+  {
+    id: 3,
+    name: "Колье «Лунный свет»",
+    category: "necklaces",
+    material: "Серебро 925",
+    gemstone: "Жемчуг",
+    price: 45600,
+    stock: 8,
+    image: "https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=400",
+    description: "Элегантное колье с жемчугом ручной работы",
+    rating: 4.7
+  },
+  {
+    id: 4,
+    name: "Браслет «Императрица»",
+    category: "bracelets",
+    material: "Золото 750",
+    gemstone: "Изумруд 2ct",
+    price: 245000,
+    stock: 3,
+    image: "https://images.unsplash.com/photo-1611591437281-460bfbe1220a?w=400",
+    description: "Роскошный браслет с изумрудами в винтажном стиле",
+    rating: 5.0
+  },
+  {
+    id: 5,
+    name: "Кольцо «Роза»",
+    category: "rings",
+    material: "Розовое золото 585",
+    gemstone: "Рубин 0.8ct",
+    price: 78900,
+    stock: 7,
+    image: "https://images.unsplash.com/photo-1603561591411-07134e71a2a9?w=400",
+    description: "Романтичное кольцо с рубином и гравировкой",
+    rating: 4.6
+  },
+  {
+    id: 6,
+    name: "Серьги «Звёзды»",
+    category: "earrings",
+    material: "Платина 950",
+    gemstone: "Бриллианты 0.3ct",
+    price: 167000,
+    stock: 4,
+    image: "https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?w=400",
+    description: "Платиновые серьги-гвоздики с россыпью бриллиантов",
+    rating: 4.9
+  },
+  {
+    id: 7,
+    name: "Подвеска «Сердце»",
+    category: "necklaces",
+    material: "Золото 585",
+    gemstone: "Аметист",
+    price: 34500,
+    stock: 15,
+    image: "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?w=400",
+    description: "Нежная подвеска в форме сердца с аметистом",
+    rating: 4.5
+  },
+  {
+    id: 8,
+    name: "Браслет «Цепь»",
+    category: "bracelets",
+    material: "Серебро 925",
+    gemstone: "Нет",
+    price: 23400,
+    stock: 20,
+    image: "https://images.unsplash.com/photo-1573408301185-9146fe634ad0?w=400",
+    description: "Массивный серебряный браслет-цепь в стиле унисекс",
+    rating: 4.4
+  }
+];
